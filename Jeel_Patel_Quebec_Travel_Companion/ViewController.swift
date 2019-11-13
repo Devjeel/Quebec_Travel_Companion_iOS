9072c238d9b33202bfa5ed0680966aa155a4bb04
Devjeel <jeelhp02@gmail.com>
2019-11-13 15:57:55 -0500

    Create Storyboard Design

--- Jeel_Patel_Quebec_Travel_Companion/ViewController.swift
+++ Jeel_Patel_Quebec_Travel_Companion/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK:- IBOutlets
+    
+    @IBOutlet weak var englishLabel: UILabel!
+    @IBOutlet weak var frenchLabel: UILabel!
+    
+    //MARK:- IBActions
+    @IBAction func buttonTouched(_ sender: UIButton) {
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
